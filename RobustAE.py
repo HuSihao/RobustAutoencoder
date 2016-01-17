e7166aa8c094ff034c047cf025597a6a6db36910
Chong <741053341@qq.com>
2016-01-17 15:33:54 -0500

    RobustAE numpy version no one norm implementation

--- RobustAE.py
+++ RobustAE.py
@@ -2,7 +2,7 @@ import timeit
 import numpy as np
 
 class RobustAE():
-    def __init__(self,hidden_size,visible_size,sparse_ratio):
+    def __init__(self,hidden_size,visible_size,sparse_ratio=0.1, random_state = 0):
         r = np.sqrt(6) / np.sqrt(hidden_size + visible_size +1)
         self.W = np.random.random((visible_size, hidden_size)) * 2 * r - r
         self.b1 = np.zeros(hidden_size , dtype= np.float64)
@@ -10,11 +10,14 @@ class RobustAE():
         self.hidden_size = hidden_size
         self.visible_size = visible_size
         self.sparse_ratio = sparse_ratio## adding sparsity
-        self.mask = np.random.random((visible_size, hidden_size)) * 2 * r - r
+        #self.mask = np.random.random((visible_size, hidden_size)) * 2 * r - r
+        self.random_state = random_state
     def sigmoid(self , x):
         return 1./(1. + np.exp(-x))
     def sigmoid_prime(self, x):
         return self.sigmoid(x)*(1. - self.sigmoid(x))
+    def one_norm_derivative(self, x):
+        return
     def split(self,x):
         sparse = x * self.mask
         return sparse , x - sparse
@@ -23,12 +26,63 @@ class RobustAE():
     def cost(self , data , lambda_):
         ##lambda_ is weight of regularization term
         samples = data.shape[0]
-        sparse , x = split(data)
+        sparse , x = self.split(data)
         z2 = np.dot(x, self.W) + np.tile(self.b1,(samples,1))
         h = self.sigmoid(z2)
         z3= np.dot(h , self.W.T)+np.tile(self.b2,(samples,1))
         re_con = self.sigmoid(z3)
 
-        cost = np.sum((re_con + sparse -data) ** 2) / samples + lambda_ * np.sum(self.W ** 2) + np.sum(sparse ** 2)
+        cost = np.sum((re_con + sparse - data) ** 2) / samples + lambda_ * np.sum(self.W ** 2)  + np.sum(abs(sparse))
         delta3 = - (re_con + sparse - data) * self.sigmoid_prime(z3)
         delta2 = np.dot(delta3, self.W) * self.sigmoid_prime(z2)
+        delta1 = - delta2 * self.mask * self.one_norm_derivative()
+
+        Wgrad = np.dot(delta3.T, h) /samples + lambda_* self.W
+        b1grad = np.sum(delta2 , axis=0) / samples
+        b2grad = np.sum(delta3 , axis=0) / samples
+        maskgrad = delta2 * x
+
+        grad = [Wgrad , b1grad , b2grad , maskgrad]
+    def one_step_training(self, data, learning_rate, lambda_):
+        one_step_cost , grad = self.cost(data, lambda_):
+        self.W = self.W - learning_rate * grad[0]
+        self.b1 = self.b1 - learning_rate * grad[1]
+        self.b2 = self.b2 - learning_rate * grad[2]
+        self.mask = self.mask - grad[3]
+        return one_step_cost
+
+    def fit(self, data, lambda_, learning_rate=0.1, iteration = 25):
+        print "initializing split.."
+        print
+        rng = np.random.RandomState(self.random_state)
+        self.mask = rng.normal(loc = 1., size = (data.shape[0], data.shape[1]))
+        print "training..."
+        print
+        training_cost = []
+        start_time = timeit.default_timer()
+
+        for i in range(iteration):
+            one_step_cost = self.one_step_training(data, 0.1 lambda_)
+            print "iteration: ", i, " cost: ", one_step_cost
+            training_cost.append(one_step_cost)
+        end_time = timeit.default_timer()
+        print
+        print "Total training time: ", (end_time - start_time)/60. , "m"
+
+    def transform(self, data):
+        sparse , x = self.split(data)
+        z2 = np.dot(x, self.W) + np.tile(self.b1,(samples,1))
+        h = self.sigmoid(z2)
+        return h
+
+if __name__== '__main__':
+    data=np.load(r'C:\Users\zc\Documents\MNIST data\Tutorial\train_x.pkl')
+    y=np.load(r'C:\Users\zc\Documents\MNIST data\Tutorial\train_y.pkl')
+    print data.shape
+    print y.shape
+    inputsize=(28,28)
+    hidden_size=(20,20)
+
+    rae = RobustAE(hidden_size=hidden_size[0]*hidden_size[1], visible_size=inputsize[0]*inputsize[1])
+    rae.fit(data,lambda_=0.5,learning_rate=0.1, iteration=2)
+    rae.transform(data)
