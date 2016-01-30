3c710ad4177af9dbc25ddfb13fac4505c405389c
Chong <741053341@qq.com>
2016-01-30 18:29:34 -0500

    BasicAE_v2.py add get_reconstruction

--- BasicAE_v2.py
+++ BasicAE_v2.py
@@ -3,7 +3,7 @@ import timeit
 import BasicAE as AE
 
 class AutoEncoder_v2(AE.AutoEncoder):
-    def getRecon(self, data, lambda_):
+    def getRecon(self, data):
         z2 = np.dot(data, self.W) + np.tile(self.b1,(samples,1))
         h = self.sigmoid(z2)
         z3= np.dot(h , self.W.T)+np.tile(self.b2,(samples,1))
